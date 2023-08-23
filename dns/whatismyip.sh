9f877760e18b5d6cd841985fee1762cc092d694f
yowgf <alexthomasmol@gmail.com>
2023-08-23 10:06:03 -0300

    Improve whatismyip

--- dns/whatismyip.sh
+++ dns/whatismyip.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-dig TXT +short o-o.myaddr.l.google.com @ns1.google.com
+resp=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com)
+if [[ "$resp" =~ ^'"' ]]; then
+  clean_resp="${resp#\"}"
+  clean_resp="${clean_resp%\"}"
+  echo "$clean_resp"
+else
+  echo "$resp"
+fi
