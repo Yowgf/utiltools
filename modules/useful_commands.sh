f82c23a1889e88e9f30663119b14b7c3d0334b29
yowgf <alexthomasmol@gmail.com>
2023-11-15 13:31:19 -0600

    Add more module and systemctl stuff

--- modules/useful_commands.sh
+++ modules/useful_commands.sh
@@ -1,3 +1,5 @@
+## MODULES #####################################################################
+
 # See currently loaded modules
 lsmod
 
@@ -6,3 +8,14 @@ find /lib/modules/$(uname -r) -type -f -name "*.ko"
 
 # Load module. Replace "rtw88_8821ce" with module name
 sudo modprobe rtw88_8821ce
+
+## PCI #########################################################################
+
+# Find wireless PCIs
+lspci | grep -i wireless
+
+# Find specific PCI
+lspci -v -s 01:00.0
+
+# Show internet devices and their status
+ip link show
--- systemctl/useful_commands.sh
+++ systemctl/useful_commands.sh
@@ -1,2 +1,5 @@
 # Check all services that are enabled (will be started upon boot)
 sudo systemctl list-unit-files --type=service --state=enabled --all
+
+# Follow system logs
+journalctl --system -f
