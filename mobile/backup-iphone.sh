57f0aad231cc4dd7af762c6fbe8b6927094fe225
yowgf <alexthomasmol@gmail.com>
2024-12-09 22:19:29 -0600

    Add backup-iphone

---
+++ mobile/backup-iphone.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+idevicepair pair
+mkdir ~/iphone
+ifuse ~/iphone/
+idevicebackup2 backup ~/iphone-backup
+idevicebackup2 info ~/iphone-backup
+
+# Note: you need the libraries from `apt install libimobiledevice-utils ifuse`
