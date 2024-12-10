2f50ea41d9c1b0edf071586cee155396fd478a03
yowgf <alexthomasmol@gmail.com>
2024-12-09 22:20:39 -0600

    Add backup-iphone

--- mobile/backup-iphone.sh
+++ mobile/backup-iphone.sh
@@ -7,3 +7,5 @@ idevicebackup2 backup ~/iphone-backup
 idevicebackup2 info ~/iphone-backup
 
 # Note: you need the libraries from `apt install libimobiledevice-utils ifuse`
+
+# To unmount, run `fusermount -u ~/iphone`
